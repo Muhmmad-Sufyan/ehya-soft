b5211fa2bb7b4d369c626d004c731549eb829ee3
suf12.jutt <suf12.jutt@gmail.com>
2026-04-16 12:30:25 +0500

    update the site url

--- src/lib/seo.js
+++ src/lib/seo.js
@@ -1,5 +1,4 @@
-// export const SITE_URL = "https://soft.ehya.com.pk";
-export const SITE_URL = "https://ehyasoft.netlify.app";
+export const SITE_URL = "https://soft.ehya.com.pk";
 export const SITE_NAME = "EhyaSoft";
 export const DEFAULT_OG_IMAGE = `${SITE_URL}/favicon.svg`;
 
